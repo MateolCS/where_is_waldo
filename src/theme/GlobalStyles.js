2aba261b23f5547290e0fa5dcbefe7a8c2fad1e6
MateoLCS <mateusz.namysl0318@gmail.com>
2022-09-26 22:43:01 +0200

    Initial commit

---
+++ src/theme/GlobalStyles.js
@@ -0,0 +1,15 @@
+import * as styled from "styled-components";
+
+const GlobalStyle = styled.createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Jost", sans-serif;
+  }
+`;
+
+export default GlobalStyle;
