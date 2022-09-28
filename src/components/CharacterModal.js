7ec35452172f5c2278a08f6da02045d3d0260f9d
MateoLCS <mateusz.namysl0318@gmail.com>
2022-09-28 22:46:58 +0200

    Added states for character modal, added styling rules

--- src/components/CharacterModal.js
+++ src/components/CharacterModal.js
@@ -1,9 +1,40 @@
 import React from "react";
 import styled from "styled-components";
-const CharacterModal = () => {
-  return <StyledCharacterModal></StyledCharacterModal>;
+
+const CharacterModal = ({ isOpen, topCord, rightCord }) => {
+  return (
+    <StyledCharacterModal
+      isOpen={isOpen}
+      topCord={topCord}
+      rightCord={rightCord}
+    >
+      <ModalContent>
+        <ModalContentItem>Waldo</ModalContentItem>
+        <ModalContentItem>WizzardBarry</ModalContentItem>
+      </ModalContent>
+    </StyledCharacterModal>
+  );
 };
 
-const StyledCharacterModal = styled.div``;
+const StyledCharacterModal = styled.div`
+  display: ${({ isOpen }) => (isOpen ? "block" : "none")};
+  position: absolute;
+  top: ${({ topCord }) => topCord}px;
+  right: ${({ rightCord }) => rightCord}px;
+`;
+
+const ModalContent = styled.nav`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ModalContentItem = styled.li`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-evenly;
+`;
 
 export default CharacterModal;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import CharacterModal from "./CharacterModal";
+import { useState } from "react";
 
 const Game = () => {
-  return <StyledGame></StyledGame>;
+  const [openMoodal, setOpenModal] = useState(false);
+  const [topCord, setTopCord] = useState(0);
+  const [rightCord, setRightCord] = useState(0);
+
+  return (
+    <StyledGame>
+      <CharacterModal
+        isOpen={openMoodal}
+        topCord={topCord}
+        rightCord={rightCord}
+      />
+    </StyledGame>
+  );
 };
 
 const StyledGame = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
+  position: relative;
 `;
 
 export default Game;
